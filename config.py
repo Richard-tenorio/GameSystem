d8562a25541f3e1279456680d0fbcfc1eecf1714
Richard Tenorio <rtenorio.k11937393@umak.edu.ph>
2025-11-06 18:12:28 +0800

    Update database config to use MySQL

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'fallback-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///gamehub.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root:@localhost/gamehub_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
     WTF_CSRF_SECRET_KEY = os.environ.get('WTF_CSRF_SECRET_KEY') or 'another-fallback-secret-key'
